b0f2c8c89768bd809f31f78733bffa820bcaaf63
Katherine <kahalliday@alaska.edu>
2024-03-20 14:27:25 -0800

    Fixed Critical Bug

--- kit.cpp
+++ kit.cpp
@@ -6,8 +6,7 @@
 
 using namespace std;
 
-//const string IN_FILE = "~/.kit/kitrc";
-const string IN_FILE = "kitrc";
+const string IN_FILE = "~/.kit/kitrc";
 
 string lookup(char** cmd) {
 	ifstream file;
